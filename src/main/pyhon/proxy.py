717cdb75bd7489b6416da9f1866fd1c87e968da8
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-01-23 22:52:32 -0200

    Changing proxy.py.

--- src/main/pyhon/proxy.py
+++ src/main/pyhon/proxy.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python
 from subprocess import *
 from sys import *
 
-command='../mathematica/src/main/proxy.m'
+command='../mathematica/proxy.m'
 parameter=argv[1]
 
 call([command,parameter])
